bbac95a7c6b984b7ae8a75dfe358624d36872187
alafyte <nroussak0405@gmail.com>
2022-12-03 19:13:07 +0300

    Лабораторная работа №5

---
+++ Lab05/Lab05/Lab05.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef unsigned char day;
+typedef unsigned char month;
+typedef unsigned short year;
+
+struct Date
+{
+    day dd;
+    month mm;
+    year yyyy;
+
+};
+
+bool operator>(Date date1, Date date2)
+{
+    if (date1.yyyy > date2.yyyy)
+        return true;
+    else if (date1.yyyy == date2.yyyy)
+    {
+        if (date1.mm > date2.mm)
+            return true;
+        else if (date1.mm == date2.mm)
+        {
+            if (date1.dd > date2.dd)
+                return true;
+            else if (date1.dd == date2.dd)
+                return false;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+};
+
+bool operator<(Date date1, Date date2)
+{
+    if (date1.yyyy < date2.yyyy)
+        return true;
+    else if (date1.yyyy == date2.yyyy)
+    {
+        if (date1.mm < date2.mm)
+            return true;
+        else if (date1.mm == date2.mm)
+        {
+            if (date1.dd < date2.dd)
+                return true;
+            else if (date1.dd == date2.dd)
+                return false;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+};
+bool operator==(Date date1, Date date2)
+{
+    if (date1.yyyy == date2.yyyy && date1.mm == date2.mm && date1.dd == date2.dd)
+        return true;
+    else return false;
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    Date date1 = { 7, 1, 1980 };
+    Date date2 = { 7, 2, 1993 };
+    Date date3 = { 7, 1, 1980 };
+
+    if (date1 < date2)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+
+    if (date1 > date2)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+
+    if (date1 == date2)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+
+    if (date1 == date3)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+
+    return 0;
+}
---
+++ Lab05/task2/task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+typedef unsigned int numbers;
+typedef string names;
+
+struct Subject
+{
+    names subjectName;
+    numbers courseNumber;
+    numbers semester;
+    numbers numberOfLectures;
+};
+
+bool operator>(Subject Subject1, Subject Subject2)
+{
+    if (Subject1.numberOfLectures > Subject2.numberOfLectures)
+        return true;
+    else return false;
+};
+
+bool operator<(Subject Subject1, Subject Subject2)
+{
+    if (Subject1.numberOfLectures < Subject2.numberOfLectures)
+        return true;
+    else return false;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    Subject Subject1 = { "Математический анализ", 1, 2, 92 };
+    Subject Subject2 = { "Основы программной инженерии", 1, 1, 46 };
+
+    if (Subject1 > Subject2)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+
+    if (Subject1 < Subject2)
+        cout << "Истина" << endl;
+    else cout << "Ложь" << endl;
+}
+
+
